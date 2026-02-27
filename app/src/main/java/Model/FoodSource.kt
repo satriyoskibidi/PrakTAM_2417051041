bd6e91e27ea21687911bd885a9f25ea153ae53f8
satriyoskibidi <poetrysatriyo@gmail.com>
2026-02-27 16:42:45 +0700

    Tugas LKP 2

---
+++ app/src/main/java/Model/FoodSource.kt
@@ -0,0 +1,9 @@
+package Model
+import com.example.praktam_2417051041.R
+
+object FoodSource {
+    val dummyFood = listOf(
+        Food("Sate", deskripsi = "Madura", harga = "100000", Imageres = R.drawable.sate),
+        Food("Nasi Goreng", deskripsi = "Pakai Babi", harga = "250000", Imageres = R.drawable.nasgor),
+    )
+}
--- app/src/main/java/com/example/praktam_2417051041/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051041/MainActivity.kt
@@ -1,9 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package com.example.praktam_2417051041
 
+import Model.Food
+import Model.FoodSource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,37 +60,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktam_2417051041.ui.theme.PrakTAM_2417051041Theme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
-class MainActivity : ComponentActivity() {
+class Ma+inActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PrakTAM_2417051041Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Satriyo Wicaksono",
-                        npm = "2417051041",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Greeting()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, npm: String) {
-    Text(
-        text = "Hello, nama saya $name dengan NPM $npm siap belajar Compose!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(){
+        val food = FoodSource.dummyFood[0]
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PrakTAM_2417051041Theme {
-        Greeting("Android", npm="2417051041")
+        Column(modifier = Modifier.fillMaxSize().padding(all=30.dp)) {+
+
+            Image(
+                painter = painterResource(id = food.Imageres),
+                contentDescription = food.nama,
+                modifier = Modifier.size(200.dp),
+                contentScale = Crop
+            )
+            Text(text = "Nama: ${food.nama}")
+            Text(text = "Deskripsi: ${food.deskripsi}")
+            Text(text = "Harga: ${food.harga}")
     }
 }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        PrakTAM_2417051041Theme {
+            Greeting(
+            )
+        }
+    }
